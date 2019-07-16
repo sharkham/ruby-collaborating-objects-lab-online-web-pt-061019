2515d5efa78aa488a029b2f5aba2fded3052ec79
Sam Markham <sam.e.markham@gmail.com>
2019-07-16 23:04:21 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -44,10 +44,20 @@ class Artist
     end
   end
 
+  #Shorter version from study group:
+  # def self.find_or_create_by_name(artist_name)
+  #   (my_artist = self.all.find {|artist| artist.name == artist_name}) ? my_artist : self.new(name)
+  # end
+
+  #Another shorter version (basically the || means do the first thing, if it's falsey, do the next thing)
+  # def self.find_or_create_by_name(artist_name)
+  #   self.all.find {|artist| artist.name == artist_name} || self.new(name)
+  # end
+
   def print_songs
     @songs.each do |song|
       puts song.name
-    end 
+    end
   end
 
 end
