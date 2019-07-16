23eb4d23aaa9b6049dd61689203cac04ed54f6ae
Sam Markham <sam.e.markham@gmail.com>
2019-07-16 22:56:58 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,6 +8,9 @@ class MP3Importer
 
   def files #this is the reader for files, can be accessed w/instance.files
     @files ||= Dir.glob("#{path}/*.mp3").collect {|f| f.gsub("#{path}/", "")}
+    #Dir.children(path) or Dir.children(self.path) can substitute in for
+    #Dir.glob("#{path}/*.mp3"). .children gets rid of the path already/shows
+    #the children under that path. 
   end
 
   def import
